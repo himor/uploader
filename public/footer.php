9e020317447b7faeffc21d6cf8b537aa32c7fb8e
Michael Gordo <himor.cre@gmail.com>
2014-11-03 21:32:09 -0500

    js fix

--- public/footer.php
+++ public/footer.php
@@ -2,7 +2,7 @@
 
 <script>
     $('#signupform').on('submit', function(event){
-        e.preventDefault();
+        event.preventDefault();
         var pasw1 = $('#pasw1').val(),
             pasw2 = $('#pasw2').val();
         if (pasw1 != pasw2) {
